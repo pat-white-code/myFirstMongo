e9078325aef7c8bcc120a29a422e8d7b2f59b694
pat-white-code <pat.k.white@gmail.com>
2020-09-11 11:16:55 -0500

    uses $size

--- mongoScripts.js
+++ mongoScripts.js
@@ -7,3 +7,22 @@
   }
 
 let proj = {_id: 0, title: 1, metacritic: 1}
+
+
+//  In this collection, watlev describes the water level at the shipwreck site and depth describes how far below sea level the ship rests. How many documents in the ships.shipwrecks collection match either of the following criteria: watlev equal to "always dry" or depth equal to 0.
+
+let filter = {
+  sections: {$all: [
+    "AG1",
+    "MD1",
+    "OA1"
+  ]}
+}
+
+let proj = {_id: 0, sections:1}
+
+// How many documents in this collection contain exactly two elements in the sections array field?
+
+let filter = {
+  sections: {$size: 2}
+}
